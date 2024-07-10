495f78598ae01e63ac686bb9e0fb00a74c93c55b
Ajay Vasu <ajay_vasu@outlook.com>
2024-07-10 06:02:14 -0700

    validated request to a small extent

--- webservice.py
+++ webservice.py
@@ -1,7 +1,27 @@
-protocol = input("Enter Protocol: ")
+import re
 
-if protocol == "HTTP" or protocol == "HTTPS":
-    print("Supported")
+
+# Validation variables
+
+httpMethods = ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS", "TRACE", "CONNECT"]
+
+# Ask for the HTTP Request
+requestMessage = input("Enter Request: ")
+
+print(requestMessage)
+
+# Parse request
+parsedRequestMessage = requestMessage.split(' ')
+method = parsedRequestMessage[0]
+URI = parsedRequestMessage[1]
+version = parsedRequestMessage[2]
+
+# Validate request
+
+if method in httpMethods:
+    print("The user would like to ", method, " ", URI, " using ", version, ".")
 else:
-    print("Protocol not supported")
+    print("Method not supported or recognized")
+    
 
+print("Success!")
