2ea84a7b22488135d745e80c556f63dd7b9b7d53
Ajay Vasu <ajay_vasu@outlook.com>
2024-07-16 20:04:52 +0530

    Corrected response messages based on request line

--- webservice.py
+++ webservice.py
@@ -9,6 +9,7 @@ def checkURI(uri):
 
 httpMethods = ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS", "TRACE", "CONNECT"]
 URIs = ["api/v1.0/countries", "/"]
+allowedVersions = ["HTTP/0.9", "HTTP/1.0", "HTTP/1.1", "HTTP/1.2", "HTTP/2"]
 
 # Ask for the HTTP Request
 requestMessage = input("Enter Request: ")
@@ -25,10 +26,13 @@ version = parsedRequestMessage[2]
 
 if method in httpMethods:
     if URI in URIs:
-        print("HTTP/1.1 200 Success - here's your data.")
+        if version in allowedVersions:
+            print("HTTP/1.1 200 Success - here's your data.")
+        else:
+            print("HTTP/1.1 400 Bad request - your protocol & version might be incorrect")
     else:
         print("HTTP/1.1 404 Resource not found")
 else:
-    print("HTTP/1.1 400 Bad Request - your specified method isn't supported")
+    print("HTTP/1.1 405 Method not allowed - your specified method isn't supported")
 
 print("Thank you for using my API :)")
